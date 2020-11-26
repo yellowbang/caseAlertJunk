145e998640c87b1839d4242fc1fb454fd882f5b2
bonster <bon@redmarlin.ai>
2020-11-25 21:54:12 -0800

    update more info

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ dotenv.config();
 const twilio = require('twilio');
 const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-const oaklandHospitals = ['Highland and Fairmont Hospitals'];
+const oaklandHospitals = ['Highland and Fairmont Hospitals', 'Alameda Hospital', 'Sutter Health'];
+const oaklandManagerAss = ['Vasquez,Angela', 'Odunikan,Kehinde', 'Lauderdale,LaRonda', 'Mulleague,Teresa',
+  'Mathiesen,Desiree', 'Fabreo,Kristine', 'Thrower,Anetra', 'Davila,Marta', 'Mulleague,Teresa', 'Lopez,Cindyann']
 
 const signInPage = async () => {
   const browser = await puppeteer.launch();
@@ -30,25 +32,33 @@ const signInPage = async () => {
       console.log('----reload table succeed----', dayjs().format());
     }
 
-    const hospitals = await page.$$eval('table#ucViewGrid_dgView>tbody>tr>td:nth-child(5)', tds => tds.map((td) => {
-      return td.innerText.split('\n')[1];
-    }));
     const infos = await page.$$eval('table#ucViewGrid_dgView>tbody>tr>td:nth-child(3)', tds => tds.map((td) => {
       const cell = td.innerText.split('\n');
       return {referralId: cell[0], dateTime: cell[2]}
     }));
+    const hospitalInfos = await page.$$eval('table#ucViewGrid_dgView>tbody>tr>td:nth-child(5)', tds => tds.map((td) => {
+      const cell = td.innerText.split('\n');
+      return {hospital: cell[1], managerAss: cell[2]};
+    }));
+    const insuranceInfos = await page.$$eval('table#ucViewGrid_dgView>tbody>tr>td:nth-child(6)', tds => tds.map((td) => {
+      const cell = td.innerText.split('\n');
+      return {insurance: cell[1]};
+    }));
 
     for (let i = 1; i < infos.length; i++) {
       const {referralId, dateTime} = infos[i];
-      const hospital = hospitals[i];
+      const {hospital, managerAss} = hospitalInfos[i];
+      const {insurance} = insuranceInfos[i];
       if (!data[referralId]) {
         data[referralId] = {
           referralId,
           dateTime,
           hospital,
+          managerAss,
+          insurance,
         }
-        if (oaklandHospitals.indexOf(hospital) !== -1 && !isInit) {
-        // if (!isInit) {
+        if (!isInit && (oaklandHospitals.indexOf(hospital) !== -1 || oaklandManagerAss.indexOf(managerAss) !== -1)) {
+          // if (!isInit) {
           console.log('----send Jojo message', isInit, JSON.stringify(data[referralId]));
           await client.messages.create({
             to: '+15102775916',
